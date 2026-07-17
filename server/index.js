d15d44d782d9380ca8e75a7676a9f12bd95eb1eb
SunilSolanki-dev <sunil.solanki.dev@gmail.com>
2026-07-17 17:00:10 +0530

    client url added in server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://aiexamnotes-u7jv.onrender.com',
   credentials:true,
   methods:["GET","POST","PUT","DELETE","OPTIONS"]
 
